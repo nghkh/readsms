3af71bf8055696309d5480cf6df8ec653287bb3d
nghkh <khoa.bach01@gmail.com>
2023-10-11 23:17:11 +0700

    first commit

--- lib/readsms.dart
+++ lib/readsms.dart
@@ -3,7 +3,6 @@ import 'readsms_platform_interface.dart';
 
 class Readsms {
   Future<String?> getPlatformVersion() {
-    print('hello');
     return ReadsmsPlatform.instance.getPlatformVersion();
   }
 }
